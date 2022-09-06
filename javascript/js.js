fec99d6dbb5d361992aa4a47357c20205a749489
HernanEspinoza <hnariel@gmail.com>
2022-09-06 12:16:55 -0300

    Sistema de entradas iniciado

---
+++ javascript/js.js
@@ -0,0 +1,17 @@
+for(let entrada = 1; entrada <= 10; entrada++) {
+    
+    let nombre = prompt("Ingrese su nombre y apellido");
+    let mensaje1 = `Entrada #${entrada} A nombre de: ${nombre}`;
+    alert(mensaje1);
+
+    
+
+    /*if(entrada === 11 ){
+        break;
+    }*/
+    
+    
+}
+
+let mensaje2 = "No hay mas entradas";
+    alert(mensaje2);
